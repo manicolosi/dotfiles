3a1196166f0b927813864fec275ada85f87ab217
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-06-07 12:35:16 -0500

    Ability for packages to have install scripts

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ PACKAGES=$(ls -d */ | xargs realpath --relative-to=$PWD)
 
 for PACKAGE in $PACKAGES; do
   echo "Stowing $PACKAGE..."
-  stow --target=$TARGET $PACKAGE
+  stow --ignore="install.sh" --target="$TARGET" "$PACKAGE"
+
+  if [ -x "$PACKAGE/install.sh" ]; then
+    echo "Running install script"
+    ./$PACKAGE/install.sh
+  fi
 done
