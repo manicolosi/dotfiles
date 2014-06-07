fc10f26f8658c0c0011233a57d31fd8a5811feb1
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-06-07 12:35:36 -0500

    Install script for vim to setup vundle

---
+++ vim/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+VUNDLE_REPO="https://github.com/gmarik/Vundle.vim.git"
+VUNDLE_DIR="$HOME/vim/bundle/Vundle.vim"
+
+if [ ! -d "$VUNDLE_DIR" ]; then
+  git clone "$VUNDLE_REPO" "$VUNDLE_DIR"
+fi
