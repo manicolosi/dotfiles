3f4a10e46e84b9b50b2e9e4392eb5186a52db75c
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2014-10-12 17:42:34 -0500

    [X] Disable left control key completely

--- X/.xprofile.d/ctrl_escape.sh
+++ X/.xprofile.d/ctrl_escape.sh
@@ -1,2 +1,13 @@
+# Make caps lock key the control modifier
 setxkbmap -option 'caps:ctrl_modifier'
+
+# Make single press on the caps lock key send the escape key
 xcape -e 'Caps_Lock=Escape'
+
+# Disable left control key
+xmodmap -e 'keycode 37='
+
+# TODO: Disable escape key
+#xmodmap -e 'keycode 9='
+
+# TODO: Should map actual left control and escape keys to some useful functions.
