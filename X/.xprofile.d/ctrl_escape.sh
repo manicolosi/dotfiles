8ffed75afaa8906fd1cc0ee8ce062594a84ddb94
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2015-02-24 20:32:06 -0600

    [X] Clarify comment regarding ctrl/escape swap

--- X/.xprofile.d/ctrl_escape.sh
+++ X/.xprofile.d/ctrl_escape.sh
@@ -7,7 +7,8 @@ xcape -e 'Caps_Lock=Escape'
 # Disable left control key
 xmodmap -e 'keycode 37='
 
-# TODO: Disable escape key
+# TODO: Disable escape key. This breaks xcape, one solution would be to map
+# escape to a keycode the keyboard cannot send first.
 #xmodmap -e 'keycode 9='
 
 # TODO: Should map actual left control and escape keys to some useful functions.
