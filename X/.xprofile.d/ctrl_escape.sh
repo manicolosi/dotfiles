e1fa4e58413f8a556df7f6c9b5e8bf8c1f8d3e94
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2015-07-29 21:10:27 -0500

    [X] Removes removal of xmodmaps that break KSP

--- X/.xprofile.d/ctrl_escape.sh
+++ X/.xprofile.d/ctrl_escape.sh
@@ -3,12 +3,3 @@ setxkbmap -option 'caps:ctrl_modifier'
 
 # Make single press on the caps lock key send the escape key
 xcape -e 'Caps_Lock=Escape'
-
-# Disable left control key
-xmodmap -e 'keycode 37='
-
-# TODO: Disable escape key. This breaks xcape, one solution would be to map
-# escape to a keycode the keyboard cannot send first.
-#xmodmap -e 'keycode 9='
-
-# TODO: Should map actual left control and escape keys to some useful functions.
