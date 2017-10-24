10036437949bb8fd447aa400fad8e10d293d1652
Joel Friedman and Mark Nicolosi <mark.a.nicolosi@gmail.com>
2017-10-24 08:48:35 -0500

    [lein] Clean up profile

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,10 +1,1 @@
-{:user
- {:plugins [[lein-droid "0.2.4-SNAPSHOT"]
-            [com.jakemccrary/lein-test-refresh "0.10.0"]]}
- :dependencies [[pjstadig/humane-test-output "0.7.0"]]
- :injections [(require 'pjstadig.humane-test-output)
-              (pjstadig.humane-test-output/activate!)]
- :android-common
- {:android
-  {:dependencies ^:replace []
-   :sdk-path "/usr/local/Cellar/android-sdk/23.0.2"}}}
+{:user {:plugins [[cider/cider-nrepl "0.15.1"]]}}
